d9e02343236289e72f347ef4433a8a8443614b16
гера шлычков <germean322@gmail.com>
2021-09-25 20:54:47 +1000

    Task5

---
+++ Task5/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace Task5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(109, 357);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(192, 23);
+            this.textBox1.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 329);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(263, 418);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 32);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Выйти";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 418);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 32);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Сгенерировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(68, 45);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(289, 233);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 517);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Task5";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Task5/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task5
+{
+    public partial class Form1 : Form
+    {
+        private Graphics _g;
+        private Pen _p;
+        private Rectangle _rectangle;
+        private int _n;
+        private const string label1Text = "Введите количество вершин полигона";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            pictureBox1.BackColor = Color.White;
+            label1.Text = label1Text;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ClearForm();
+
+            var nResult = int.TryParse(textBox1.Text, out _n);
+
+            if (!nResult || _n < 2)
+            {
+                MessageBox.Show("Не правильное количество вершин", "Ошибка");
+
+                return;
+            }
+
+            _g = Graphics.FromHwnd(pictureBox1.Handle);
+
+            _p = new Pen(Color.Red);
+
+            _rectangle = new Rectangle(50, 10, 200, 200);
+
+            var points = MakeRandomPolygon(_n, _rectangle);
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                if (i == points.Length - 1)
+                {
+                    _g.DrawLine(_p, points[i], points[0]);
+                }
+                else
+                {
+                    _g.DrawLine(_p, points[i], points[i + 1]);
+                }
+                
+            }
+
+            for (int i = 1; i < points.Length; i++)
+            {
+                _g.DrawLine(_p, points[0], points[i]);
+            }
+        }
+
+        public PointF[] MakeRandomPolygon(int num_vertices,
+            Rectangle bounds)
+        {
+            // Выбор случайных радиусов.
+            double[] radii = new double[num_vertices];
+            const double min_radius = 0.5;
+            const double max_radius = 1.0;
+
+            for (int i = 0; i < num_vertices; i++)
+            {
+                radii[i] = NextDouble(min_radius, max_radius);
+            }
+
+            // Выбор случайных угловых весов.
+            double[] angle_weights = new double[num_vertices];
+            const double min_weight = 1.0;
+            const double max_weight = 10.0;
+            double total_weight = 0;
+
+            for (int i = 0; i < num_vertices; i++)
+            {
+                angle_weights[i] = NextDouble(min_weight, max_weight);
+                total_weight += angle_weights[i];
+            }
+
+            // Преобразование весов во фракции 2 * Pi радианов.
+            double[] angles = new double[num_vertices];
+            double to_radians = 2 * Math.PI / total_weight;
+
+            for (int i = 0; i < num_vertices; i++)
+            {
+                angles[i] = angle_weights[i] * to_radians;
+            }
+
+            // Вычислить местоположения точек.
+            PointF[] points = new PointF[num_vertices];
+            float rx = bounds.Width / 2f;
+            float ry = bounds.Height / 2f;
+            float cx = bounds.MidX();
+            float cy = bounds.MidY();
+            double theta = 0;
+
+            for (int i = 0; i < num_vertices; i++)
+            {
+                points[i] = new PointF(
+                    cx + (int)(rx * radii[i] * Math.Cos(theta)),
+                    cy + (int)(ry * radii[i] * Math.Sin(theta)));
+                theta += angles[i];
+            }
+
+            // Вернем точки.
+            return points;
+        }
+
+        public double NextDouble(double a, double b) // считаем случайное дробное число в промежутке от до
+        {
+            Random random = new Random();
+
+            double x = random.NextDouble();
+
+            return x * a + (1 - x) * b;
+        }
+
+        private void ClearForm()
+        {
+            if (_g is not null)
+            {
+                _g.Clear(Color.White);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) =>
+            Close();
+    }
+}
